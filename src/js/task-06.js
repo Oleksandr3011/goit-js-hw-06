85261dc6fd307b15eeb098050ce6457a9500127f
Oleksandr3011 <115745256+Oleksandr3011@users.noreply.github.com>
2022-12-24 20:00:18 +0200

    completed 6 tasks

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -22,3 +22,18 @@
 // #validation-input.invalid {
 //   border-color: #f44336;
 // }
+
+const formInput = document.querySelector("input");
+
+formInput.addEventListener("blur", (event) => {
+  let evCurTar = event.currentTarget;
+  const toggleFunc = (a, b) => {
+    evCurTar.classList.add(a);
+    evCurTar.classList.remove(b);
+  };
+  if (evCurTar.value.length !== +formInput.getAttribute("data-length")) {
+    toggleFunc("invalid", "valid");
+  } else {
+    toggleFunc("valid", "invalid");
+  }
+});
